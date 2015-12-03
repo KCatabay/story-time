f4bc550a82d013fb04076728148c54dbeabd2eb1
KCatabay <kcatabay@gmail.com>
2015-12-02 22:55:34 -0500

    added styles, html, and inline js

---
+++ js/story.js
@@ -0,0 +1,13 @@
+var noun = prompt('Please type a noun'); 
+var adjective = prompt('Please type an adjective');
+var verb = prompt('Please type a verb'); 
+
+var story = 'There once was a ' 
+    story += noun
+    story += ' that was '
+    story += verb
+    story += ' on a '
+    story += adjective 
+    story += ' hill.'
+    
+document.write(story);
